25346f6679539ea41b0b8fd10e1c7cdc2bae9df7
Pablo Varela <pablovarela182@gmail.com>
2017-08-26 21:38:00 +0200

    Login, register, forms, footer, flex fix, etc

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,9 @@ import $ from 'jquery'
 window.$ = window.jQuery = $
 
 const postBoxClicked = () => window.location.href = '/post.html'
+const goBackButtonClicked = () => window.history.back()
+const goTopButtonClicked = () => $('body').animate({scrollTop: 0}, 200)
 
 $(document).on('click', '.post-box', postBoxClicked)
+$(document).on('click', '.go-back-button', goBackButtonClicked)
+$(document).on('click', '.go-top', goTopButtonClicked)
